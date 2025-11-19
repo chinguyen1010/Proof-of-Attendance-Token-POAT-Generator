3b4442f4b5288bf0a943d55505fa1992251b09f9
Chi L. Nguyen <chi2006@bu.edu>
2025-11-19 07:03:34 -0500

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    // add testnet here if you want (e.g., sepolia)
+  }
+};
